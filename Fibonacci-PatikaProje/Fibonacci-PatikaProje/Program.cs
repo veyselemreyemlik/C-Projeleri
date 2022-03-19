6e319a894349103032049dc2f5390a288f3ec982
Veysel Emre Yemlik <emreyemlik1905@gmail.com>
2022-03-19 23:48:39 +0300

    Add files via upload

---
+++ Fibonacci-PatikaProje/Fibonacci-PatikaProje/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci_PatikaProje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> fibonacci = new List<int>() { 0, 1 };
+            int n = 0;
+            float fiboToplam = 0;
+            float fiboOrt = 0;
+
+            try
+            {
+                Console.WriteLine("Lütfen Fibonacci Serisinin Derinliğini Belirtiniz :");
+                n = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+            while (n > fibonacci.Count)
+            {
+                fibonacci.Add(fibonacci[fibonacci.Count - 1] + fibonacci[fibonacci.Count - 2]);
+            }
+
+            foreach (var i in fibonacci)
+            {
+                fiboToplam += i;
+            }
+
+            fiboOrt = fiboToplam / fibonacci.Count;
+            Console.WriteLine("Ortalama =>" + " " + fiboToplam);
+
+        }
+    }
+}
