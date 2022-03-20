1461684f3ba0c78884f845d5d07b30a0501c1b5b
Veysel Emre Yemlik <emreyemlik1905@gmail.com>
2022-03-20 22:38:48 +0300

    Add files via upload

---
+++ Ucgen-Cizim/Ucgen-Cizim/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ucgen_Cizim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************Düz Üçgen****************");
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                
+            }
+        }
+    }
+}
