56a1d88959448cb2f94b7651036925ff9e82560a
Veysel Emre Yemlik <emreyemlik1905@gmail.com>
2022-03-21 22:07:53 +0300

    Add files via upload

---
+++ Karakter-Tersten-Yazdırma/Karakter-Tersten-Yazdırma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Karakter_Tersten_Yazdırma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //metin.Length "-1" deme sebebimiz diziler
+            //diziler "0". indisden başlamaktır
+
+            Console.Write("Lütfen bir metinleri giriniz : "  );
+            string metin1 = Console.ReadLine();
+           
+            for (int i = metin1.Length - 1; i >= 0; i--)
+            {
+              Console.Write(metin1[i]);
+            }
+
+        }
+    }
+}
