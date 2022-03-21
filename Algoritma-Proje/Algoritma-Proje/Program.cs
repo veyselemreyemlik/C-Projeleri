3607c034a9c08faab9aecfce7dfd37becabcdfcd
Veysel Emre Yemlik <emreyemlik1905@gmail.com>
2022-03-21 21:29:18 +0300

    Add files via upload

---
+++ Algoritma-Proje/Algoritma-Proje/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Algoritma_Proje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input : Selam,4
+            // Output : Sela
+            // Girilen sayi değerinden itibaren diğer indeksler Remove metodu ile silinir.
+
+            Console.Write("Bir Kelime Giriniz :");
+            string yazi  = Console.ReadLine();
+
+            Console.Write("Bir Sayı Giriniz :");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            
+            string sonYazi = yazi + "," +sayi.ToString();
+            Console.WriteLine("Yazı + Sayı : "+ " " +sonYazi);
+
+            yazi = yazi.Remove(sayi);
+            Console.WriteLine(yazi);
+
+        }
+    }
+}
